4e203e0cfac28e4d0a5834ef5dc8ff859674e6f0
Anand Raj <anandraj6783@gmail.com>
2026-08-31 19:59:23 +0530

    feat(runtime): implement language abstraction

---
+++ internal/runtime/runtime.go
@@ -0,0 +1,28 @@
+package runtime
+
+import "fmt"
+
+type Runtime interface {
+	Image() string
+	Extension() string
+	Command(fileName string) []string
+}
+
+// GetRuntime is a function that takes a string name
+// and returns a specific Runtime implementation.
+func GetRuntime(name string) (Runtime, error) {
+	switch name {
+	case "python":
+		return &PythonRuntime{
+			"python:3.11-alpine",
+			".py",
+		}, nil
+	case "go":
+		return &GoRuntime{
+			"golang:1.21-alpine",
+			".go",
+		}, nil
+	default:
+		return nil, fmt.Errorf("unsupported runtime: %s", name)
+	}
+}
