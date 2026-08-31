698043f6cf5f7dbd88b2e9c3e7ba3dd5c3bf9ccf
Anand Raj <anandraj6783@gmail.com>
2026-08-31 19:58:53 +0530

    feat(runtime): implement language  support for python

---
+++ internal/runtime/python.go
@@ -0,0 +1,17 @@
+package runtime
+
+type PythonRuntime struct {
+	image     string
+	extension string
+}
+
+func (p *PythonRuntime) Image() string {
+	return p.image
+}
+
+func (p *PythonRuntime) Extension() string {
+	return p.extension
+}
+func (p *PythonRuntime) Command(filename string) []string {
+	return []string{"python", filename}
+}
