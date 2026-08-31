8422e61a98846dc7e518f09657552632079a323a
Anand Raj <anandraj6783@gmail.com>
2026-08-31 19:58:44 +0530

    feat(runtime): implement language  support for go

---
+++ internal/runtime/go.go
@@ -0,0 +1,17 @@
+package runtime
+
+type GoRuntime struct {
+	image     string
+	extension string
+}
+
+func (g *GoRuntime) Image() string {
+	return g.image
+}
+
+func (g *GoRuntime) Extension() string {
+	return g.extension
+}
+func (g *GoRuntime) Command(filename string) []string {
+	return []string{"go", "run", filename}
+}
