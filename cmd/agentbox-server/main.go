37620f01abd07ee2a0683eaae4e5e3e347d468eb
Anand Raj <anandraj6783@gmail.com>
2026-09-03 21:23:51 +0530

    feat(server): inject orchestrator dependency into grpc api

--- cmd/agentbox-server/main.go
+++ cmd/agentbox-server/main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/AnandRaj2224/agentbox/internal/api"
 	"github.com/AnandRaj2224/agentbox/internal/config"
 	"github.com/AnandRaj2224/agentbox/internal/logger"
+	"github.com/AnandRaj2224/agentbox/internal/orchestrator"
 	"google.golang.org/grpc"
 )
 
@@ -35,9 +36,15 @@ func main() {
 		log.Error("error listen on port", config.Port, err)
 		return
 	}
-
+	
+	cli, err := orchestrator.New(log)
+	if err != nil {
+		log.Error("error creating docker client", "error", err)
+		return
+	}
+	
 	grpcServer := grpc.NewServer()
-	api.RegisterExecutionServiceServer(grpcServer, api.NewServer(log))
+	api.RegisterExecutionServiceServer(grpcServer, api.NewServer(log, cli))
 	go grpcServer.Serve(listener)
 
 	<-ctx.Done()
