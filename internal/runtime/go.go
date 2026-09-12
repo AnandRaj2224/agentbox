e69733937190faf0b48c35df85a486b0fc0d12ce
Anand Raj <anandraj6783@gmail.com>
2026-09-12 13:14:04 +0530

    docs: disable module

--- internal/runtime/go.go
+++ internal/runtime/go.go
@@ -13,5 +13,6 @@ func (g *GoRuntime) Extension() string {
 	return g.extension
 }
 func (g *GoRuntime) Command(filename string) []string {
+	// Disable module tracking so the compiler can execute a standalone file
 	return []string{"sh", "-c", "GO111MODULE=off go run /" + filename}
 }
